3197480bbb0dd4c29ed1409e2e094290679ba5cf
Ben Broadley <>
2019-03-28 08:27:56 +0000

    Styled the leaderboard table so that it looks nicer.

--- my-project/src/components/LeaderBoard.vue
+++ my-project/src/components/LeaderBoard.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
-    <pre class="text-left">{{ output }}</pre>
+    <div v-for="(groupPlayers, group) in output" :key="group" class="row">
+      <b-table striped hover :items="groupPlayers"></b-table>
+    </div>
+
   </div>
 </template>
 
 <script>
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-underscore-dangle */
 import axios from 'axios';
-import { map } from 'lodash';
+import { map, forEach } from 'lodash';
 
 export default {
   name: 'LeaderBoard',
@@ -19,6 +24,12 @@ export default {
   created() {
     axios.get('http://localhost:4000/game')
       .then((res) => {
+        const outputs = res.data;
+        forEach(outputs, (players) => {
+          forEach(players, (player, idx) => {
+            if (idx <= 1) player._rowVariant = 'success';
+          });
+        });
         this.output = res.data;
       })
       .catch((e) => {
